26fade75e09598e089416d126c097b0292fc9769
Matheus Valadão <matheusnvaladao@gmail.com>
2022-06-02 17:52:19 -0300

    up

---
+++ FutLiveServer/Api/Models/ClassificacaoRetorno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutLiveServer.Models
+{
+    public class ClassificacaoRetorno
+    {
+        public string clube_nome { get; set; }
+        public string clube_image { get; set; }
+        public int clube_posicao { get; set; }
+    }
+}
